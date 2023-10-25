8e2c56f3838205a0b1bcb7f88b7b4b1ff32b09dd
Dilshod Muxtorov <d.muxtorov@newuu.uz>
2023-10-25 20:24:02 +0500

    codes

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,21 +1,38 @@
 /*
- * Author:
- * Date:
+ * Author:Muxtorov Dilshod
+ * Date:25.10.2023
  */
 
 #include <iostream>
 #include "task_1.h"
 
+
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
+    // std::cout << "Task 1" << std::endl;
+    // int year;
+    // std::cout<<"Enter a year: ";
+    // std::cin>>year;
+    // if(is_leap(year)){
+    //     std::cout<<"This year is leap!";
+    // }else{
+    //     std::cout<<"This year is not leap!";
+    // }
+
     std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    int random_number = rand()%100, guess_number=0;
+    pin:
+    std::cout<<"Guess the number(between 1 and 100): ";
+    std::cin>>guess_number;
+    if(random_number>guess_number){
+        std::cout<<"too low"<< std::endl;
+        goto pin;
+    }else if (random_number<guess_number){
+        std::cout<<"too high"<< std::endl;
+        goto pin;
+    }else {
+        std::cout<<"You find the number!";
+    }
+
+
     return 0;
 }
--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,41 @@
-# * Author:
-# * Date:
+# * Author:Muxtorov Dilshod
+# * Date:25.10.2023
+
+#  3-Task
+# import random
+# A = int(input("Enter a number: "))
+# number = ""
+# for i in range(100):
+#     digit = str(random.randint(0, 9))
+#     number += digit
+# result = int(number) // A
+# print("The result is:", result)
+
+
+
+# 4 task
+
+# def  boolean(sentence:str,word:str):
+#     sentence=sentence.split(" ")
+#     for i in range(len(sentence)+1):
+#         if word == sentence[i]:
+#             result = i
+#             break
+#         else:
+#             result= "No match"
+#     return result
+    
+# sentence = input("Enter a sentence: ")
+# word = input("Enter a word: ")
+# print(boolean(sentence,word))
+
+# 5-task
+
+
+
+
+
+
+
+
+
